49af7d70fef2b5c8a230d97d09c5e555acc75b03
Sameer Siruguri <siruguri@gmail.com>
2013-09-28 23:01:11 -0700

    Query params php code

--- code/01.queryparams.php
+++ code/01.queryparams.php
@@ -2,9 +2,15 @@
 <html>
 
 <body>
+<h1>GET</h1>
 <?php
-echo "You passed in sizeof($_GET) parameters";
-echo "The value of parameter <B>greeting</b> is $_GET['greeting']";
+echo ("You passed in " . sizeof($_GET) . " parameters<br/>");
+echo("In your GET params, the value of parameter <B>greeting</b> is ". $_GET['greeting']);
+?>
+<h1>POST</h1>
+<?php
+echo ("You passed in " . sizeof($_POST) . " parameters<br/>");
+echo("In your POST params, the value of parameter <B>greeting</b> is ". $_POST['greeting']);
 ?>
 
 </body>
