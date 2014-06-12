b3f1f9453b9591bdbfa6ab5cd2c8c0338d798227
Sameer Siruguri <siruguri@gmail.com>
2014-06-12 09:56:23 -0700

    redid blogger_simple in rails4; html forms

--- code/blogger_simple/app/controllers/main_controller.rb
+++ code/blogger_simple/app/controllers/main_controller.rb
@@ -1,8 +1,12 @@
-class ThisWontWorkMainController < ActionController::Base
+class MainController < ActionController::Base
   protect_from_forgery
 
   def commands
     @the_rest = params[:the_rest]
     @output_var = request.path
+
+    @input_params = (params.map do |k,v|
+                       "#{k}: #{v}"
+                     end).join(",")
   end
 end
--- code/blogger_simple/config/initializers/secret_token.rb
+++ code/blogger_simple/config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-BloggerSimple::Application.config.secret_token = '431017e9718b100fc9c52285bb57714c6bcb5946fef0538858029582376f96d4f7c4cfc82d74e5815da9adcc1e77d7febe68fb9612dcbfbdbd9864d96bbebefc'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BloggerSimple::Application.config.secret_key_base = 'c4abfbab43df57853265ff212f2cf7c2ee8ec8c2fcca53db0c32cfb5a81f34265cc833333bce62850b64794757abcb1a297595d916227c002844a0f16c6db743'
